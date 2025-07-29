7fd2be0aa7ebfbfb08a8b6b457be668e0f988d91
nico lesca <nicolescano004@gmail.com>
2025-07-29 00:05:21 -0300

    Arregle errores sobre la comunicacion de las rutas a la base de datos. Agrege test cases

--- data_base.py
+++ data_base.py
@@ -95,4 +95,3 @@ def borrar():
     conn.close()
 
 
-tabla("movies")
--- rutas.py
+++ rutas.py
@@ -1,11 +1,11 @@
 from data_base import tabla, agregar_fila, agregar_varios, actualizar, seleccionar, eliminar_fila
 from modelos import Peli, Ides, Tabla
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from fastapi import HTTPException
 
 router = APIRouter()
 
-@router.post("/", response_model=Ides)
+@router.post("/add", response_model=Ides)
 def movie_add(pelicula:Peli):
     try:
         add = agregar_fila(pelicula.title, pelicula.director, pelicula.year)
@@ -13,23 +13,23 @@ def movie_add(pelicula:Peli):
         raise HTTPException(status_code=409, detail=str(e)) 
     return Ides(mensaje=f"Pelicula agregada, la ID es: {add}")
 
-@router.patch("/")
-def update_movie(columna, dato, ide):
+@router.patch("/update")
+def update_movie(columna: str = Query(...), dato: str = Query(...), ide: int = Query(...)):
     try:
         actualizar(columna, dato, ide)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
     return{"Server message":"Fila actualizada"}
 
-@router.get("/")
-def select_movies(columna, valor):
+@router.get("/select")
+def select_movies(columna: str = Query(...), valor: str = Query(...)):
     try:
         resultado = seleccionar(columna, valor)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
     return resultado
 
-@router.delete("/")
+@router.delete("/delete")
 def movie_delete(pelicula:Peli):
     try:
         eliminar_fila(pelicula.title, pelicula.director, pelicula.year)
